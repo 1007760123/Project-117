aad2aa10d5d60a3a5f55394f68e4f0019721badf
1007760123 <96152753+1007760123@users.noreply.github.com>
2022-01-19 18:41:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+quick_draw_data_set = ["aircraft carrier", "airplane", "alarm clock", "ambulance", "angel", "animal_migration", "ant", "anvil", "apple", "arm", "asparagus", "axe", "backpack", "banana", "barn", "baseball", "baseball bat", "basket", "basketball", "bat", "bathtub", "beach", "bear", "beard", "bed", "bee", "belt", "bench", "bycicle", "binoculars", "bird", "birthday cake", "blackberry", "blueberry", "book", "boomerang", "bottlecap", "bowtie"];
+array_1 = ['pen', 'paper', 'book', 'bottle']
+random_number = Math.floor((Math.random()*array_1.length)+1);
+Element_of_array = array_1[random_no];
+console.log(array_1[random_no]);
